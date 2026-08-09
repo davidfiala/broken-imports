014f18b018fe6d25057a3716ae3ab8e6ef367884
David Fiala <davidfiala@users.noreply.github.com>
2026-08-09 21:34:56 +0000

    minimal reproduction with devcontainer

---
+++ src/main.ts
@@ -0,0 +1,6 @@
+import { createHash } from "node:crypto";
+
+// ------------------------------------------------------------------------------------------------
+// Command palette manualy invoked: > TypeScript: Remove Unused Imports
+// Expected: The unused import for createHash should be removed from this file.
+// ------------------------------------------------------------------------------------------------
